58979d54cfd71998dfed6a5c3b582655826ecb1a
simonfoy <simonfoy1@proton.me>
2023-10-12 14:23:18 -0400

    Added GameRule GUI for Players
    Introduced a GUI interface that allows players to view server game rules.
    - Added a method `openGameRuleGUI` which creates an inventory with a display item for game rules.
    - Modified `onCommand` to call the GUI opening method when a player sends the command.

--- src/main/java/me/simonfoy/gamerulegui/command/GameRuleGUICommand.java
+++ src/main/java/me/simonfoy/gamerulegui/command/GameRuleGUICommand.java
@@ -1,17 +1,36 @@
 package me.simonfoy.gamerulegui.command;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class GameRuleGUICommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            // TODO: Open the GUI for the player
+            openGameRuleGUI(player);
         }
         return true;
+    }
+
+    private void openGameRuleGUI(Player player) {
+        Inventory gui = Bukkit.createInventory(null, 27, "Your Server Settings");
+
+        ItemStack gameRulesItem = new ItemStack(Material.ENCHANTED_BOOK);
+        ItemMeta gameRulesItemMeta = gameRulesItem.getItemMeta();
+        gameRulesItemMeta.setDisplayName(ChatColor.GREEN + "Game Rules");
+        gameRulesItem.setItemMeta(gameRulesItemMeta);
+
+        gui.setItem(13, gameRulesItem);
+
+        player.openInventory(gui);
     }
 }
