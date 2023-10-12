65cab8218f19b0b3df7f9179afeb1ae81042f940
simonfoy <simonfoy1@proton.me>
2023-10-12 14:07:41 -0400

    Implement command to open GUI for players
    Added a new command executor (`GameRuleGUICommand`) which will enable players to trigger the opening of a GUI. The actual GUI opening logic is yet to be implemented.

--- src/main/java/me/simonfoy/gamerulegui/GameRuleGUI.java
+++ src/main/java/me/simonfoy/gamerulegui/GameRuleGUI.java
@@ -1,12 +1,13 @@
 package me.simonfoy.gamerulegui;
 
+import me.simonfoy.gamerulegui.command.GameRuleGUICommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GameRuleGUI extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.getCommand("gamerulegui").setExecutor(new GameRuleGUICommand());
 
     }
 
---
+++ src/main/java/me/simonfoy/gamerulegui/command/GameRuleGUICommand.java
@@ -0,0 +1,17 @@
+package me.simonfoy.gamerulegui.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GameRuleGUICommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            // TODO: Open the GUI for the player
+        }
+        return true;
+    }
+}
